06ad15b435f4362ca30a60eae8cd26432e47ba73
Dariusz Witowski <dariusz.witowski@statsperform.com>
2026-02-17 14:08:10 +0100

    paths update

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,24 @@ import Portfolio from './pages/Portfolio/Portfolio';
 import About from './pages/About/About';
 
 function App() {
-  const { i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
   const location = useLocation();
 
   // Update HTML lang attribute when language changes
   useEffect(() => {
     document.documentElement.lang = i18n.language;
   }, [i18n.language]);
+
+  // Update page title based on current route
+  useEffect(() => {
+    const getPageTitle = () => {
+      const path = location.pathname;
+      if (path === '/portfolio') return t('pageTitle.portfolio');
+      if (path === '/about') return t('pageTitle.about');
+      return t('pageTitle.home');
+    };
+    document.title = getPageTitle();
+  }, [location.pathname, t]);
 
   // Scroll to top on route change
   useEffect(() => {
