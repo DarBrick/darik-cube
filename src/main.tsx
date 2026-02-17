a37d8142bce8cf6fc978bfae449f1513cce32109
Dariusz Witowski <dariusz.witowski@statsperform.com>
2026-02-17 13:32:46 +0100

    change path serving

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,9 @@ import './index.css'
 import './i18n/config'
 import App from './App.tsx'
 
-// Get basename from window (set by base path detection script in index.html)
-const basename = (window as any).__BASE_PATH__ || '/';
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename={basename}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
